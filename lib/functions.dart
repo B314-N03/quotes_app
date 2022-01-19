8c5b6791c3f9ae58626cdbe81d7f944f8d5f0421
B314-Win10 <bela-noe@web.de>
2022-01-19 18:03:01 +0100

    added fetchQuotes function to fetch the data from API

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,7 +1,11 @@
-import 'package:flutter/cupertino.dart';
+import 'package:quotes_app/quotes_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'favourite.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+String errorMsg = "";
+bool error = false;
 
 Future<List<String>> loadData() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -13,4 +17,18 @@ Future<List<String>> loadData() async {
 void deleteListContent(List<dynamic> pList) {
   favoQuotes.clear();
   favoQuotesSaved.clear();
+}
+
+Future<Quotes> fetchQuotes() async {
+  final response = await http.get(
+      Uri.parse('https://api.quotable.io/random?minLength=40&maxLength=70'));
+
+  if (response.statusCode == 200) {
+    error = false;
+    return Quotes.fromJson(jsonDecode(response.body));
+  } else {
+    errorMsg =
+        "Something went Wrong... Check your internet connection and Try again!";
+    throw Exception();
+  }
 }
